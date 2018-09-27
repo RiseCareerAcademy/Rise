bec893fb3c0e6f1c017f57756b8d6e73ed111529
quyminh <ffate97@gmail.com>
2018-09-26 23:57:55 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Minh is a dumbass
+              Mui is a dumbass MEOW MEOW MEOEW.
             </Text>
           </View>
 
