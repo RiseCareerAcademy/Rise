18a89ac37c6e8bfc3ab6f36ea42475dbe2dfecf3
Rita Roloff <ritaroloff@Ritas-MacBook-Pro.local>
2018-10-20 21:27:10 -0500

    adding hardcoded profile screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import Profile1 from '../screens/Profile1'
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  
+  // Main: MainTabNavigator,
+  Main: Profile1,
 });
