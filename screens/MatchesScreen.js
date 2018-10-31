aff2c1d2684b74eff9d93ef479f2142530350772
Kevin Mui <kevinmui@Kevins-MacBook-Pro.local>
2018-10-30 22:16:17 -0500

    fixed matches screen

--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -10,6 +10,35 @@ export default class MatchesScreen extends Component {
     scores: []
   };
 
+  constructor(props) {
+    super(props);
+
+    //fakeData organized by iterations of (name, profession, skills);
+    const fakeData = [
+      ["Daniel Ng"],
+      ["Software Developer"],
+      ["JQuery", "Python", "UX"], //should be number 4
+      ["Tracy Lewis"],
+      [""],
+      ["UX", "Prototyping"], //should be number 3
+      ["Kevin Mui"],
+      ["Product Manager"],
+      [""], //should be number 2
+      ["Lewis Tracy"],
+      ["Product Manager"],
+      ["UX"], //should be number 1
+      ["Tone Yu"],
+      ["Nurse"],
+      ["Birthing", "Yelling", "Running"] //should not be in the results
+    ];
+
+    const { desiredSkills, desiredProfessions } = this.state;
+    const { scores, matches } = this.match(desiredSkills, desiredProfessions, fakeData);
+
+    this.state.scores = scores;
+    this.state.matches = matches;
+  }
+
   //match function
   match = (desiredSkills, desiredProfessions, fakeData) => {
     const matches = [];
@@ -60,32 +89,12 @@ export default class MatchesScreen extends Component {
       }
     }
 
-    this.setState({ scores, matches });
+    return { scores, matches };
   }
 
   render() {
-    //fakeData organized by iterations of (name, profession, skills);
-    const fakeData = [
-      ["Daniel Ng"],
-      ["Software Developer"],
-      ["JQuery", "Python", "UX"], //should be number 4
-      ["Tracy Lewis"],
-      [""],
-      ["UX", "Prototyping"], //should be number 3
-      ["Kevin Mui"],
-      ["Product Manager"],
-      [""], //should be number 2
-      ["Lewis Tracy"],
-      ["Product Manager"],
-      ["UX"], //should be number 1
-      ["Tone Yu"],
-      ["Nurse"],
-      ["Birthing", "Yelling", "Running"] //should not be in the results
-    ];
 
-    const { desiredSkills, desiredProfessions, matches, scores } = this.state;
-
-    this.match(desiredSkills, desiredProfessions, fakeData);
+    const { matches, scores } = this.state;
 
     return (
       <View style={styles.container}>
