eb455930dc172719d3a7677ddade68c8f4dc7d09
meenereem <dng4@wisc.edu>
2018-11-01 18:26:59 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,46 +1,36 @@
 import React from "react";
 import { Button, Text } from "native-base";
-import { ScrollView, StyleSheet, Image, View } from "react-native";
-import { AuthSession } from "expo";
+import { Platform, ScrollView, StyleSheet, Image, View } from "react-native";
+import { AuthSession, ImagePicker, Permissions } from "expo";
+import { MonoText } from "../components/StyledText";
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      result: null
+      result: null,
     };
   }
 
-  hello = () => {
-    return 'hello';
-  }
 
   handleMentorPress = async () => {
     // Setup params for Linkedin API
     // For more details: https://developer.linkedin.com/docs/oauth2
-    const response_type = "code";
-    const client_id = "7872jtsnbo9n7s";
+    const response_type = 'code';
+    const client_id = '7872jtsnbo9n7s';
     const redirectUrl = AuthSession.getRedirectUrl();
-    const state =
-      Math.random()
-        .toString(36)
-        .substring(2, 15) +
-      Math.random()
-        .toString(36)
-        .substring(2, 15);
+    const state =  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     const authUrl =
-      `https://www.linkedin.com/oauth/v2/authorization?response_type=${response_type}` +
-      `&client_id=${encodeURIComponent(client_id)}` +
-      `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
-      `&state=${encodeURIComponent(state)}`;
+    `https://www.linkedin.com/oauth/v2/authorization?response_type=${response_type}` +
+    `&client_id=${encodeURIComponent(client_id)}` +
+    `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+    `&state=${encodeURIComponent(state)}`;
 
     // Use Expo's AuthSession to connect with the Linkedin API
     // For more details: https://i.expo.io/versions/latest/sdk/auth-session
     const result = await AuthSession.startAsync({ authUrl });
-    const {
-      params: { state: responseState, code }
-    } = result;
+    const { params: { state: responseState, code } } = result;
     let validState = true;
     if (responseState !== state) {
       validState = false;
@@ -48,7 +38,7 @@ export default class HomeScreen extends React.Component {
 
     // This only displays the results to the screen
     this.setState({ result, authUrl, validState, responseState, state });
-  };
+  }
 
   render() {
     const { navigate } = this.props.navigation;
@@ -64,38 +54,38 @@ export default class HomeScreen extends React.Component {
               style={{ height: 200, width: 200 }}
             />
           </View>
-          <Button full dark onPress={this.handleMentorPress} title="Mentor" />
+            <Button full dark onPress={this.handleMentorPress}>
+              <Text>Mentor</Text>
+            </Button>
 
           <View>
-            <Button
-              full
-              light
-              onPress={() => navigate("Student")}
-              title="Student"
-              style={styles.greyText}
-            />
+            <Button full light onPress={() =>
+              navigate('Student')
+            }>
+              <Text style={styles.greyText}>Student</Text>
+            </Button>
           </View>
           <View style={styles.center}>
-            <Text>--- Or ---</Text>
+            <Text>
+            --- Or ---
+            </Text>
           </View>
           <View>
-            <Button full dark onPress={() => navigate("SignIn")} title="SIGN IN" />
-            {this.state.validi !== undefined ? (
-              <Text>
-                {JSON.stringify(this.state.validState) +
-                  "\nRequestState: " +
-                  this.state.state +
-                  "\nResponseState: " +
-                  this.state.responseState}
-              </Text>
-            ) : null}
-            {this.state.authUrl ? (
-              <Text>{JSON.stringify(this.state.authUrl)}</Text>
-            ) : null}
-            {this.state.result ? (
-              <Text>{JSON.stringify(this.state.result)}</Text>
-            ) : null}
-          </View>
+          <Button full dark onPress={() =>
+            navigate('SignIn')
+          }>
+            <Text>SIGN IN</Text>
+          </Button>
+          {this.state.validi !== undefined ? (
+          <Text>{JSON.stringify(this.state.validState) + '\nRequestState: ' + this.state.state + '\nResponseState: ' + this.state.responseState}</Text>
+          ) : null}
+          {this.state.authUrl ? (
+          <Text>{JSON.stringify(this.state.authUrl)}</Text>
+          ) : null}
+          {this.state.result ? (
+          <Text>{JSON.stringify(this.state.result)}</Text>
+          ) : null}
+        </View>
         </ScrollView>
       </View>
     );
@@ -109,9 +99,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center"
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   center: {
     flex: 1,
@@ -126,5 +116,5 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingTop: 30
-  }
+  },
 });
