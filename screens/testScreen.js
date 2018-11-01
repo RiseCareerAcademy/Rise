b5bae3fd6c0d43461f0433d321ec3ad3fa17fd0c
Rita Roloff <ritaroloff@Ritas-MacBook-Pro.local>
2018-11-01 14:15:52 -0500

    made a loop

--- screens/testScreen.js
+++ screens/testScreen.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { TouchableOpacity, View, ActivityIndicator, Text, Alert} from 'react-native';
 export default class App extends Component {
-    state ={
-        mentors:[[]]
-    }
+    
 handlePress = async () => {
   fetch('http://192.168.0.104:8000/user/mentor', {
       method: 'GET'
@@ -11,7 +9,14 @@ handlePress = async () => {
     .then((response) => response.json())
     .then((responseJson) => {
         //TO DO: add mentors name and skills in array
+        var mentors = [];
+        for (var i = 0; i < responseJson.rows.length; i++){
+            var curr_row = responseJson.rows[i];
+            mentors.push([curr_row.first_name],[curr_row.skills]);
+       }
  console.log(responseJson.rows[0].first_name + "'s skills are  " + responseJson.rows[0].skills);
+ console.log(responseJson.rows.length);
+ console.log(mentors.toString())
     })
     .catch((error) => {
       console.log("error is: " + error);
