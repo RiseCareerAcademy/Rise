128e132f4a1f020013537f36e4c871ea03e37423
Anna H <hadjiev@wisc.edu>
2018-11-02 22:12:06 -0300

    Search (#108)
    * added search function
    
    * Added search function to search bar in profile screen

--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -7,39 +7,36 @@ export default class MatchesScreen extends Component {
     desiredProfessions: ["Product Manager"], //temp
     //array of matches with respective scores
     matches: [],
-    scores: [],
-    mentorsFromServer:[]
+    scores: []
   };
 
   constructor(props) {
     super(props);
-    var mentors = [];
-    //populate array with data from database
 
-    
-    //replace 10.141.17.61 with your own ip address
-    fetch('http://10.141.17.61:8000/user/mentor', {
-      method: 'GET'
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-        //TO DO: add mentors name and skills in array
-        
-        for (var i = 0; i < responseJson.rows.length; i++){
-            var curr_row = responseJson.rows[i];
-            mentors.push([curr_row.first_name])
-            mentors.push([curr_row.occupation])
-            mentors.push( curr_row.skills);
-       }
-      console.log(mentors.join('\n'))
-      const { desiredSkills, desiredProfessions } = this.state;
-      const { scores, matches } = this.match(desiredSkills, desiredProfessions, mentors);
-      this.setState({scores: scores});
-      this.setState({matches: matches});
-    })
-    .catch((error) => {
-      console.log("error is: " + error);
-    });
+    //fakeData organized by iterations of (name, profession, skills);
+    const fakeData = [
+      ["Daniel Ng"],
+      ["Software Developer"],
+      ["JQuery", "Python", "UX"], //should be number 4
+      ["Tracy Lewis"],
+      [""],
+      ["UX", "Prototyping"], //should be number 3
+      ["Kevin Mui"],
+      ["Product Manager"],
+      [""], //should be number 2
+      ["Lewis Tracy"],
+      ["Product Manager"],
+      ["UX"], //should be number 1
+      ["Tone Yu"],
+      ["Nurse"],
+      ["Birthing", "Yelling", "Running"] //should not be in the results
+    ];
+
+    const { desiredSkills, desiredProfessions } = this.state;
+    const { scores, matches } = this.match(desiredSkills, desiredProfessions, fakeData);
+
+    this.state.scores = scores;
+    this.state.matches = matches;
   }
 
   //match function
