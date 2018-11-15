e35941527057ba2f10b17ce37a1f73416affacd3
meenereem <dng4@wisc.edu>
2018-11-14 19:14:24 -0600

    match

--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { ScrollView, StyleSheet, View, SectionList, Text } from "react-native";
 import Expo from "expo";
 import { Graph } from 'react-d3-graph';
+import { get } from "mongoose";
 
 export default class MatchesScreen extends Component {
   state = {
@@ -65,51 +66,64 @@ export default class MatchesScreen extends Component {
     // ]))
     const matches = [];
     const scores = [];
-
-    for (let i = 0; i < desiredSkills.length; i++) {
-      for (let j = 0; j < fakeData.length; j += 3) {
-        if (fakeData[j + 2].indexOf(desiredSkills[i]) > -1) {
-          //if matching skill, add to match list, score ++
-          if (matches.indexOf(fakeData[j]) > -1)
-            //if already has points
-            scores[matches.indexOf(fakeData[j])] += 1;
-          else {
-            //else add to list of matches with new score
-            matches[matches.length] = fakeData[j];
-            scores[scores.length] = 1;
-          }
-        }
-      }
+    const menteeNode = {id: , profession: desiredProfessions, skills: desiredSkills};
+    const mentorNodes = [id: , ];
+    let i;
+    //get user by profession, loop through users and add to mentorNodes if they are mentor
+    
+    //go through skills
+    for (i = 0; i < desiredSkills.length; i++){
+      //get users by skills, loop through users and check if they are mentor then add to mentorNodes
     }
 
-    for (let i = 0; i < desiredProfessions.length; i++) {
-      for (let j = 0; j < fakeData.length; j += 3)
-        if (fakeData[j + 1].indexOf(desiredProfessions[i]) > -1) {
-          if (matches.indexOf(fakeData[j]) > -1)
-            //if matching skill, add to match list, score ++
-            //if already has points
-            scores[matches.indexOf(fakeData[j])] += 3;
-          else {
-            //else add to list of matches with new score
-            matches[matches.length] = fakeData[j];
-            scores[scores.length] = 3;
-          }
-        }
+    for (i = 0; i < mentee.skills; i++){
+      
     }
 
-    //sort by score
-    for (let i = 1; i < scores.length; i++) {
-      for (let j = 0; j < i; j++) {
-        if (scores[i] > scores[j]) {
-          let x = scores[i];
-          scores[i] = scores[j];
-          scores[j] = x;
-          let y = matches[i];
-          matches[i] = matches[j];
-          matches[j] = y;
-        }
-      }
-    }
+    // for (let i = 0; i < desiredSkills.length; i++) {
+    //   for (let j = 0; j < fakeData.length; j += 3) {
+    //     if (fakeData[j + 2].indexOf(desiredSkills[i]) > -1) {
+    //       //if matching skill, add to match list, score ++
+    //       if (matches.indexOf(fakeData[j]) > -1)
+    //         //if already has points
+    //         scores[matches.indexOf(fakeData[j])] += 1;
+    //       else {
+    //         //else add to list of matches with new score
+    //         matches[matches.length] = fakeData[j];
+    //         scores[scores.length] = 1;
+    //       }
+    //     }
+    //   }
+    // }
+
+    // for (let i = 0; i < desiredProfessions.length; i++) {
+    //   for (let j = 0; j < fakeData.length; j += 3)
+    //     if (fakeData[j + 1].indexOf(desiredProfessions[i]) > -1) {
+    //       if (matches.indexOf(fakeData[j]) > -1)
+    //         //if matching skill, add to match list, score ++
+    //         //if already has points
+    //         scores[matches.indexOf(fakeData[j])] += 3;
+    //       else {
+    //         //else add to list of matches with new score
+    //         matches[matches.length] = fakeData[j];
+    //         scores[scores.length] = 3;
+    //       }
+    //     }
+    // }
+
+    // //sort by score
+    // for (let i = 1; i < scores.length; i++) {
+    //   for (let j = 0; j < i; j++) {
+    //     if (scores[i] > scores[j]) {
+    //       let x = scores[i];
+    //       scores[i] = scores[j];
+    //       scores[j] = x;
+    //       let y = matches[i];
+    //       matches[i] = matches[j];
+    //       matches[j] = y;
+    //     }
+    //   }
+    // }
 
     return { scores, matches };
   }
