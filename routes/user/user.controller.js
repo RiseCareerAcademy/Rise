5764a9e5ffff71f72c259edc0df9d3abadc4db38
Kevin Mui <kevinmui@Kevins-MacBook-Pro.local>
2018-11-15 18:02:13 -0600

    cleaning up code

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -3,26 +3,25 @@ const path = require("path");
 const fs = require('fs');
 const config = require("../../config/database.js");
 const db = require('../../db');
-var date = new Date();
 
 const os = require('os');
 const ip = require('ip');
 const { getRandomArbitrary } = require('../../utils/getRandomArbitrary');
 
 
-var user_sql_constants = require("../../config/user_sql_constants.js");
-var hp = require("../../config/helper.js");
+const user_sql_constants = require("../../config/user_sql_constants.js");
+const hp = require("../../config/helper.js");
 
 //create all tables
 module.exports.createTables = (req, res) => {
 
-  sql1 = user_sql_constants.create_mentor_table_sql();
-  sql2 = user_sql_constants.create_mentee_table_sql();
-  sql3 = user_sql_constants.create_password_table_sql();
-  sql4 = user_sql_constants.create_matches_table_sql();
-  sql5 = user_sql_constants.create_messages_table_sql();
-  sql6 = user_sql_constants.create_skills_table_sql();
-  sql7 = user_sql_constants.create_professions_table_sql();
+  const sql1 = user_sql_constants.create_mentor_table_sql();
+  const sql2 = user_sql_constants.create_mentee_table_sql();
+  const sql3 = user_sql_constants.create_password_table_sql();
+  const sql4 = user_sql_constants.create_matches_table_sql();
+  const sql5 = user_sql_constants.create_messages_table_sql();
+  const sql6 = user_sql_constants.create_skills_table_sql();
+  const sql7 = user_sql_constants.create_professions_table_sql();
 
   db.all(sql1, [], (err, rows) => {
     if (err) {
@@ -62,15 +61,15 @@ module.exports.createTables = (req, res) => {
   res.json({ success: true, rows: "all tables created" });
 }
 //drop table 
-module.exports.deletetable = (req, res) => {
+module.exports.deleteTable = (req, res) => {
 
-  sql1 = `DROP TABLE IF EXISTS Mentors;`;
-  sql2 = `DROP TABLE IF EXISTS Mentees;`;
-  sql3 = `DROP TABLE IF EXISTS Passwords;`;
-  sql4 = `DROP TABLE IF EXISTS Matches;`;
-  sql5 = `DROP TABLE IF EXISTS Messages;`;
-  sql6 = `DROP TABLE IF EXISTS Skills;`;
-  sql7 = `DROP TABLE IF EXISTS Profession;`;
+  const sql1 = `DROP TABLE IF EXISTS Mentors;`;
+  const sql2 = `DROP TABLE IF EXISTS Mentees;`;
+  const sql3 = `DROP TABLE IF EXISTS Passwords;`;
+  const sql4 = `DROP TABLE IF EXISTS Matches;`;
+  const sql5 = `DROP TABLE IF EXISTS Messages;`;
+  const sql6 = `DROP TABLE IF EXISTS Skills;`;
+  const sql7 = `DROP TABLE IF EXISTS Profession;`;
 
   db.all(sql1, [], (err, rows) => {
     if (err) {
@@ -118,7 +117,7 @@ module.exports.postMentor = (req, res) => {
     'date_of_birth', 'profession', 'skills', 'profile_pic_URL', 'hobbies'];
   const user = {};
 
-  fields.forEach(field => {
+  const missingFields = fields.some(field => {
     if (req.body[field] === undefined) {
       res
         .status(500)
@@ -126,9 +125,14 @@ module.exports.postMentor = (req, res) => {
       return true;
     }
     user[field] = req.body[field];
+    return false;
   });
 
-  sql_email = `Select * from Mentors where email_address = ?  `
+  if (missingFields) {
+    return;
+  }
+
+  const sql_email = `Select * from Mentors where email_address = ?  `
   db.all(sql_email, [user.email_address], (err, rows_email) => {
     if (err) {
       throw err;
@@ -140,11 +144,9 @@ module.exports.postMentor = (req, res) => {
     }
 
 
-    date = new Date()
-    userID = parseInt(10000000000000 + date.getTime());
-    //console.log(userID)
-    sql = `INSERT INTO Mentors VALUES (?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?) `
-    console.log(sql);
+    const date = new Date()
+    const userID = parseInt(10000000000000 + date.getTime());
+    const sql = `INSERT INTO Mentors VALUES (?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?) `
     db.all(sql, [userID, user.first_name, user.last_name, user.email_address,
       user.biography, user.zipcode, user.date_of_birth, user.profession,
       user.skills, user.profile_pic_URL, user.hobbies], (err, rows) => {
@@ -154,14 +156,14 @@ module.exports.postMentor = (req, res) => {
       });
 
     //add user ID to skills table 
-    skill = user.skills;
-    sql3 = `SELECT users FROM Skills WHERE skills = ?`;
+    const skill = user.skills;
+    const sql3 = `SELECT users FROM Skills WHERE skills = ?`;
     db.all(sql3, [skill], (err, rows3) => {
       if (err) {
         throw err;
       }
       if (rows3.length == 0) {
-        sql4 = `INSERT INTO Skills VALUES (?,CAST(? AS int))`
+        const sql4 = `INSERT INTO Skills VALUES (?,CAST(? AS int))`
         db.all(sql4, [skill, parseInt(userID)], (err, rows4) => {
           if (err) {
             throw err;
@@ -169,10 +171,9 @@ module.exports.postMentor = (req, res) => {
           console.log("insert users into new skill")
         });
       } else {
-        users = rows3[0]['users']
-        users = addToString(users, userID);
+        const users = addToString(rows3[0]['users'], userID);
 
-        sql5 = `UPDATE Skills SET users = ? WHERE skills = ?`
+        const sql5 = `UPDATE Skills SET users = ? WHERE skills = ?`
         db.all(sql5, [users, skill], (err, rows5) => {
           if (err) {
             throw err;
@@ -183,15 +184,15 @@ module.exports.postMentor = (req, res) => {
     });
 
     //add user ID to profession table 
-    profession = user.profession;
-    sql3 = `SELECT users FROM Profession WHERE profession = ?`;
+    const profession = user.profession;
+    const sql33 = `SELECT users FROM Profession WHERE profession = ?`;
     db.all(sql3, [profession], (err, rows3) => {
       if (err) {
         throw err;
       }
       //this means profession doesnt exist 
       if (rows3.length == 0) {
-        sql4 = `INSERT INTO Profession VALUES (?,CAST(? AS int))`
+        const sql4 = `INSERT INTO Profession VALUES (?,CAST(? AS int))`
         db.all(sql4, [profession, userID], (err, rows4) => {
           if (err) {
             throw err;
@@ -201,10 +202,9 @@ module.exports.postMentor = (req, res) => {
       }
       //profession exists, append the user to list  
       else {
-        users = rows3[0]['users']
-        users = addToString(users, userID)
+        const users = addToString(rows3[0]['users'], userID)
         //add the users to the profession 
-        sql5 = `UPDATE Profession SET users = ? WHERE profession = ?`
+        const sql5 = `UPDATE Profession SET users = ? WHERE profession = ?`
         db.all(sql5, [users, profession], (err, rows5) => {
           if (err) {
             throw err;
@@ -222,16 +222,22 @@ module.exports.postMentee = (req, res) => {
   const fields = ['first_name', 'last_name', 'email_address', 'biography', 'zipcode',
     'date_of_birth', 'skills', 'profession', 'profile_pic_URL', 'hobbies'];
   const user = {};
-  fields.forEach(field => {
+  const missingFields = fields.some(field => {
     if (req.body[field] === undefined) {
       res
         .status(500)
         .json({ error: "Missing credentials", success: false });
+      return true;
     }
     user[field] = req.body[field];
+    return false;
   });
 
-  sql_email = `Select * from Mentees where email_address = ?  `
+  if (missingFields) {
+    return;
+  }
+
+  const sql_email = `Select * from Mentees where email_address = ?  `
   db.all(sql_email, [user.email_address], (err, rows_email) => {
     if (err) {
       throw err;
@@ -241,10 +247,9 @@ module.exports.postMentee = (req, res) => {
       res.json({ success: false, rows: "Email is not unique" });
       return;
     }
-    date = new Date()
-    userID = parseInt(20000000000000 + date.getTime());
-    sql = `INSERT INTO Mentees VALUES (?,?,?,?,?,?,?,?,?,?,?) `
-    console.log(sql);
+    const date = new Date()
+    const userID = parseInt(20000000000000 + date.getTime());
+    const sql = `INSERT INTO Mentees VALUES (?,?,?,?,?,?,?,?,?,?,?) `
     db.all(sql, [userID, user.first_name, user.last_name, user.email_address, user.biography,
       user.zipcode, user.date_of_birth, user.profession, user.skills, user.profile_pic_URL, user.hobbies], (err, rows) => {
         if (err) {
@@ -253,14 +258,14 @@ module.exports.postMentee = (req, res) => {
         res.json({ success: true, rows: rows });
       });
     //add user ID to skills table 
-    skill = user.skills;
-    sql3 = `SELECT users FROM Skills WHERE skills = ?`;
+    const skill = user.skills;
+    const sql3 = `SELECT users FROM Skills WHERE skills = ?`;
     db.all(sql3, [skill], (err, rows3) => {
       if (err) {
         throw err;
       }
       if (rows3.length == 0) {
-        sql4 = `INSERT INTO Skills VALUES (?,CAST(? AS int))`
+        const sql4 = `INSERT INTO Skills VALUES (?,CAST(? AS int))`
         db.all(sql4, [skill, parseInt(userID)], (err, rows4) => {
           if (err) {
             throw err;
@@ -268,10 +273,9 @@ module.exports.postMentee = (req, res) => {
           console.log("insert users into new skill")
         });
       } else {
-        users = rows3[0]['users']
-        users = addToString(users, userID);
+        const users = addToString(rows3[0]['users'], userID);
 
-        sql5 = `UPDATE Skills SET users = ? WHERE skills = ?`
+        const sql5 = `UPDATE Skills SET users = ? WHERE skills = ?`
         db.all(sql5, [users, skill], (err, rows5) => {
           if (err) {
             throw err;
@@ -282,8 +286,7 @@ module.exports.postMentee = (req, res) => {
     });
 
     //add user ID to profession table 
-    profession = user.profession;
-    console.log(profession, userID)
+    const { profession } = user;
     sql3 = `SELECT users FROM Profession WHERE profession = ?`;
     db.all(sql3, [profession], (err, rows3) => {
       if (err) {
@@ -291,7 +294,7 @@ module.exports.postMentee = (req, res) => {
       }
       //this means profession doesnt exist 
       if (rows3.length == 0) {
-        sql4 = `INSERT INTO Profession VALUES (?,CAST(? AS int))`
+        const sql4 = `INSERT INTO Profession VALUES (?,CAST(? AS int))`
         db.all(sql4, [profession, userID], (err, rows4) => {
           if (err) {
             throw err;
@@ -301,10 +304,9 @@ module.exports.postMentee = (req, res) => {
       }
       //profession exists, append the user to list  
       else {
-        users = rows3[0]['users']
-        users = addToString(users, userID)
+        const users = addToString(rows3[0]['users'], userID)
         //add the users to the profession 
-        sql5 = `UPDATE Profession SET users = ? WHERE profession = ?`
+        const sql5 = `UPDATE Profession SET users = ? WHERE profession = ?`
         db.all(sql5, [users, profession], (err, rows5) => {
           if (err) {
             throw err;
@@ -336,9 +338,7 @@ module.exports.postMessage = (req, res) => {
   if (missingFields) {
     return;
   }
-  console.log(user)
-  sql = `INSERT INTO Messages VALUES ('${date.getTime()}',?,?,?,?,?)`
-  console.log(date.getTime())
+  const sql = `INSERT INTO Messages VALUES ('${date.getTime()}',?,?,?,?,?)`
   db.all(sql, [user.match_id, user.to_id, user.from_id, user.message_body, getFormattedDate()], (err, rows) => {
     if (err) {
       throw err;
@@ -372,10 +372,9 @@ module.exports.postPassword = (req, res) => {
       return;
     }
   });
-  var salt = hp.genRandomString(16)
-  var passwordData = hp.saltPassword(user.password, salt)
+  const salt = hp.genRandomString(16)
+  const passwordData = hp.saltPassword(user.password, salt)
   sql = `INSERT INTO Passwords VALUES (?,'${passwordData.passwordHash}', '${passwordData.salt}') `
-  console.log(sql);
   db.all(sql, [user.email_address], (err, rows) => {
     if (err) {
       throw err;
@@ -400,7 +399,6 @@ module.exports.postSkill = (req, res) => {
     return false;
   });
   sql = `INSERT INTO Skills VALUES (?, ?) `
-  console.log(sql);
   db.all(sql, [skill.skill, skill.users], (err, rows) => {
     if (err) {
       throw err;
@@ -424,7 +422,6 @@ module.exports.postProfession = (req, res) => {
     return false;
   });
   sql = `INSERT INTO Profession VALUES (?, ?) `
-  console.log(sql);
   db.all(sql, [profession.profession, profession.users], (err, rows) => {
     if (err) {
       throw err;
@@ -836,7 +833,6 @@ module.exports.updateProfession = (req, res) => {
         throw err;
       }
 
-      console.log("old", old_profession)
       //seing if there was an old profession (there should always be)
       if (rows3.length == 0) {
         res.json({ success: true, rows: "Profession not found !" });
@@ -994,13 +990,11 @@ module.exports.login = (req, res) => {
           .status(400)
           .json({ error: "Wrong password", success: false });
       }
-      console.log(rows2[0].email_address)
       sql3 = `SELECT user_id FROM Mentors WHERE email_address=? UNION SELECT user_id FROM Mentees WHERE email_address=?`
       db.all(sql3, [user.email_address, user.email_address], (err, rows3) => {
         if (err) {
           throw err;
         }
-        console.log(rows3)
         res.json({ success: true, rows: rows3 });
       })
     });
@@ -1012,12 +1006,10 @@ module.exports.login = (req, res) => {
 
 module.exports.getMessages = (req, res) => {
   sql = `SELECT * FROM Messages;`
-  console.log("here")
   db.all(sql, [], (err, rows) => {
     if (err) {
       throw err;
     }
-    console.log(rows)
     res.json({ success: true, rows: rows });
   });
 }
