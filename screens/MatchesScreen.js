63c7b3f6a1e2407197471784877baddc76dd8bcd
Kevin Mui <kevinmui@Kevins-MacBook-Pro.local>
2018-11-22 01:07:29 -0600

    turned matches list into cards

--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
-import { List, ListItem } from 'react-native-elements';
-import { View } from "react-native";
-import { connect } from 'react-redux';
+import { View, RefreshControl } from "react-native";
+import { connect } from "react-redux";
 
-import { getMatches } from '../actions/matches.actions';
-import { Header } from "native-base";
+import { getMatches } from "../actions/matches.actions";
+import {
+  Header,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Thumbnail,
+  Body,
+  Text,
+  Right,
+  Button,
+  Container,
+  Card,
+  CardItem,
+  Image,
+  Icon,
+} from "native-base";
 
 export class MatchesScreen extends Component {
   static navigationOptions = {
     header: null,
-  }
-  
+  };
+
   state = {
     desiredSkills: "",
     desiredProfession: "",
     //array of matches with respective scores
     matches: [],
     list: [],
-    error: "hi"
+    error: "hi",
+    refreshing: false,
   };
 
   constructor(props) {
@@ -25,33 +41,71 @@ export class MatchesScreen extends Component {
 
     this.props.getMatches();
   }
-  
-  render () {
+
+  componentDidUpdate = prevProps => {
+    if (prevProps.mentors !== this.props.mentors) {
+      this.setState({ refreshing: false });
+    }
+  };
+
+  onRefresh = () => {
+    this.props.getMessages();
+    this.setState({ refreshing: true });
+  };
+
+  render() {
     return (
-      <View>
-          <Header />
+      <Container>
+        <Header />
+        <Content
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh}
+            />
+          }
+        >
           <List>
-            {
-              this.props.mentors.map(l => (
-                <ListItem
-                  roundAvatar
-                  key={l.user_id}
-                  avatar={{ uri: l.profile_pic_URL }}
-                  title={`${l.first_name} ${l.last_name}`}
-                  subtitle={`${l.profession} | ${l.skills} | Score: ${l.score}`}
-                />
-              ))
-            }
+            {this.props.mentors.map(l => (
+              <Card style={{ flex: 0 }} key={l.user_id}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{ uri: l.profile_pic_URL }} />
+                    <Body>
+                      <Text>{`${l.first_name} ${l.last_name}`}</Text>
+                      <Text note>{l.profession}</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text>{l.biography}</Text>
+                    <Text>{l.skills}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Button transparent textStyle={{ color: "#87838B" }}>
+                      <Text>{`Score: ${l.score}`}</Text>
+                    </Button>
+                  </Left>
+                </CardItem>
+              </Card>
+            ))}
           </List>
-      </View>
-    )
+        </Content>
+      </Container>
+    );
   }
 }
 
 const mapStateToProps = state => ({
   mentors: state.matches.mentors,
-})
+});
 
-export default connect(mapStateToProps, {
-  getMatches,
-})(MatchesScreen);
+export default connect(
+  mapStateToProps,
+  {
+    getMatches,
+  }
+)(MatchesScreen);
