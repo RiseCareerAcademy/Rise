751569056c5dd9df29dc91351f07d6aaeb04db98
Kevin Mui <kevinmui@Kevins-MacBook-Pro.local>
2018-11-22 22:27:21 -0600

    fixed reset table endpoint

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -1,10 +1,13 @@
+const path = require("path");
+const fs = require("fs");
+const { Expo } = require('expo-server-sdk');
 const dbPromise = require("../../db");
 const sql = require("sql-template-strings");
+var date = new Date();
 
 const ip = require("ip");
 const axios = require("axios");
 const qs = require("qs");
-let date = new Date();
 
 const SQL = require("../../config/user_sql_constants.js");
 const hp = require("../../config/helper.js");
@@ -20,7 +23,7 @@ module.exports.createTables = async (req, res) => {
       db.run(SQL.CREATE_MATCHES_TABLE),
       db.run(SQL.CREATE_MESSAGES_TABLE),
       db.run(SQL.CREATE_SKILLS_TABLE),
-      db.run(SQL.CREATE_PROFESSIONS_TABLE)
+      db.run(SQL.CREATE_PROFESSIONS_TABLE),
     ]);
     res.json({ success: true, rows: "all tables created" });
   } catch (e) {
@@ -47,6 +50,9 @@ module.exports.resetTable = async (req, res) => {
       db.run(dropMessagesTableSql),
       db.run(dropSkillsTableSql),
       db.run(dropProfessionTableSql),
+    ]);
+
+    await Promise.all([
       db.run(SQL.CREATE_USERS_TABLE),
       db.run(SQL.CREATE_PASSWORD_TABLE),
       db.run(SQL.CREATE_MATCHES_TABLE),
@@ -54,7 +60,7 @@ module.exports.resetTable = async (req, res) => {
       db.run(SQL.CREATE_SKILLS_TABLE),
       db.run(SQL.CREATE_PROFESSIONS_TABLE)
     ]);
-    
+
     res.json({ success: true, rows: "reset all tables" });
   } catch (e) {
     console.error(e.message);
@@ -77,7 +83,7 @@ module.exports.linkedin = async (req, res) => {
       qs.stringify(requestBody)
     );
     const {
-      data: { access_token, expires_in },
+      data: { access_token },
     } = response;
     const config = {
       headers: {
@@ -204,7 +210,7 @@ module.exports.postMentor = async (req, res) => {
         await db.run(updateSkillSql);
       }
     }
-    const selectUsersByProfessionSql = sql`SELECT users FROM Profession WHERE profession = 
+    const selectUsersByProfessionSql = sql`SELECT users FROM Profession WHERE profession =
           ${user.profession}`;
     const usersWithProfessionObject = await db.get(selectUsersByProfessionSql);
 
@@ -296,7 +302,7 @@ module.exports.postMentee = async (req, res) => {
         await db.run(updateSkillSql);
       }
     }
-    const selectUsersByProfessionSql = sql`SELECT users FROM Profession WHERE profession = 
+    const selectUsersByProfessionSql = sql`SELECT users FROM Profession WHERE profession =
           ${user.profession}`;
     const usersWithProfessionObject = await db.get(selectUsersByProfessionSql);
 
@@ -351,15 +357,131 @@ module.exports.postMessage = async (req, res) => {
   }
 };
 
+module.exports.registerPushToken = (req, res) => {
+  const { pushToken } = req.body;
+  if (pushToken === undefined) {
+    res.status(500).json({ error: "Missing credentials", success: false });
+    return;
+  }
+  const pushTokens = [pushToken];
+  // Create a new Expo SDK client
+  const expo = new Expo();
+
+  // Create the messages that you want to send to clents
+  const messages = [];
+  pushTokens.forEach( pushToken => {
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+    } else {
+      // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+      messages.push({
+        to: pushToken,
+        sound: 'default',
+        body: 'This is a test notification',
+        data: { withSome: 'data' },
+      })
+    }
+
+  });
+
+  // The Expo push notification service accepts batches of notifications so
+  // that you don't need to send 1000 requests to send 1000 notifications. We
+  // recommend you batch your notifications to reduce the number of requests
+  // and to compress them (notifications with similar content will get
+  // compressed).
+  const chunks = expo.chunkPushNotifications(messages);
+  const tickets = [];
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+
+
+
+
+    // Later, after the Expo push notification service has delivered the
+    // notifications to Apple or Google (usually quickly, but allow the the service
+    // up to 30 minutes when under load), a "receipt" for each notification is
+    // created. The receipts will be available for at least a day; stale receipts
+    // are deleted.
+    //
+    // The ID of each receipt is sent back in the response "ticket" for each
+    // notification. In summary, sending a notification produces a ticket, which
+    // contains a receipt ID you later use to get the receipt.
+    //
+    // The receipts may contain error codes to which you must respond. In
+    // particular, Apple or Google may block apps that continue to send
+    // notifications to devices that have blocked notifications or have uninstalled
+    // your app. Expo does not control this policy and sends back the feedback from
+    // Apple and Google so you can handle it appropriately.
+    let receiptIds = [];
+    for (let ticket of tickets) {
+      // NOTE: Not all tickets have IDs; for example, tickets for notifications
+      // that could not be enqueued will have error information and no receipt ID.
+      if (ticket.id) {
+        receiptIds.push(ticket.id);
+      }
+    }
+
+    let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+    (async () => {
+      // Like sending notifications, there are different strategies you could use
+      // to retrieve batches of receipts from the Expo service.
+      for (let chunk of receiptIdChunks) {
+        try {
+          let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+          console.log(receipts);
+
+          // The receipts specify whether Apple or Google successfully received the
+          // notification and information about an error, if one occurred.
+          for (let receipt of receipts) {
+            if (receipt.status === 'ok') {
+              continue;
+            } else if (receipt.status === 'error') {
+              console.error(`There was an error sending a notification: ${receipt.message}`);
+              if (receipt.details && receipt.details.error) {
+                // The error codes are listed in the Expo documentation:
+                // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+                // You must handle the errors appropriately.
+                console.error(`The error code is ${receipt.details.error}`);
+              }
+            }
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
+
+}
+
 const sockets = {};
 
 //create a new message
 module.exports.conversation = (ws, req) => {
   const { from_id } = req.query;
-  console.log('');
   sockets[from_id] = ws;
   ws.on('message', stringifiedMessage => {
     console.log(`received ws message: ${stringifiedMessage}`);
+
+
     const message = JSON.parse(stringifiedMessage);
     const { to_id } = message;
 
@@ -425,7 +547,7 @@ module.exports.postPassword = async (req, res) => {
 
     const insertPasswordSql = sql`INSERT INTO Passwords VALUES (
       ${user.email_address},
-      '${passwordData.passwordHash}', 
+      '${passwordData.passwordHash}',
       '${passwordData.salt}'
     ) `;
 
@@ -951,8 +1073,8 @@ module.exports.login = async (req, res) => {
     const saltObject = await db.get(getSaltSql);
     const salt = saltObject["salt"]
     const loginSql = sql`SELECT u.user_id FROM Users u INNER JOIN
-    (SELECT email_address FROM Passwords  
-    WHERE email_address = ${user.email_address} 
+    (SELECT email_address FROM Passwords
+    WHERE email_address = ${user.email_address}
     AND password = ${hp.saltPassword(user.password, salt)["passwordHash"]}) p
     ON (u.email_address = p.email_address)
     ;`;
@@ -990,7 +1112,7 @@ module.exports.getLatestMessagesById = async (req, res) => {
     const db = await dbPromise;
     const matchID = req.params.matchid;
     const getAllMessagesSql =
-      sql`SELECT * FROM Messages WHERE match_id = ${matchID} 
+      sql`SELECT * FROM Messages WHERE match_id = ${matchID}
     AND timestamp <=  ${req.body.timestamp} ORDER BY timestamp LIMIT ${req.body.limit};`;
     const messagesRows = await db.all(getAllMessagesSql);
     res.json({ success: true, rows: messagesRows });
