2d9ba056e7826dc9281b5ac730286f482f68e6bc
Kevin Mui <kevinmui@Kevins-MacBook-Pro.local>
2018-11-27 02:58:25 -0600

    added domain url env usage

--- config/url.js
+++ config/url.js
@@ -3,4 +3,4 @@ import Expo from "expo";
 const { manifest } = Expo.Constants;
 export const DOMAIN = process.env.NODE_ENV === 'development' ?
 	`${manifest.debuggerHost.split(`:`)[0]}:8000` :
-	`api.example.com`;
+	process.env.DOMAIN_URL;
